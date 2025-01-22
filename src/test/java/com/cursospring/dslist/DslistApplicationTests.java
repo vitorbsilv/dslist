daeeb26b0dad7a5613b9693b2544d3baf075a453
Vitor <vitor.barboza876@gmail.com>
2025-01-22 12:09:44 -0300

    Project created

---
+++ src/test/java/com/cursospring/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
